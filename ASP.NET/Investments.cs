9bfc5e1792d4667143ca065c20dc50a17a66c935
BedirT <mtapkan@na.edu>
2018-02-11 16:14:36 -0600

    HW1 ASP.NET added

---
+++ ASP.NET/Investments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Investments
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int inv = 10000; // Investment money
+            int year = 0;
+            while (inv < 1000000){
+                inv += inv * 15 / 100;
+                year++;
+            }
+            Console.WriteLine(year);
+        }
+    }
+}
---
+++ ASP.NET/Paycheck/Paycheck/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Paycheck
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+        public void done_clicked(object sender, EventArgs args)
+        {
+            int res_wage, res_work, res;
+
+            if (int.TryParse(hourly_wage.Text, out res_wage)){
+                if(int.TryParse(hours_worked.Text, out res_work)){
+                    if(res_work > 40){
+                        res_work += ((res_work - 40) / 2);
+                    }
+                    res = res_wage * res_work;    
+                    label_2.Text = "Total: " + res;
+                }else{
+                    hours_worked.Text = "Please insert correct value for weight.";
+                }
+            } else{
+                hourly_wage.Text = "Please insert correct value for weight.";
+            }
+        }
+    }
+}
